6514f26f8356b8303309eb7a72592d6410697766
marianolozu <marianolozu@gmail.com>
2022-05-02 12:19:21 +0200

    [WIP] Cambios en la clase Plantas para añadir nuevos atributos de clase.

--- src/classes/Plantas.java
+++ src/classes/Plantas.java
@@ -1,4 +1,29 @@
 package classes;
 
 abstract class Plantas {
+
+    private String colorPetalo;
+    private String tipoMaceta;
+
+    public Plantas(String colorPetalo, String tipoMaceta) {
+        this.colorPetalo = colorPetalo;
+        this.tipoMaceta = tipoMaceta;
+    }
+
+    public String getColorPetalo() {
+        return colorPetalo;
+    }
+
+    public void setColorPetalo(String colorPetalo) {
+        this.colorPetalo = colorPetalo;
+    }
+
+    public String getTipoMaceta() {
+        return tipoMaceta;
+    }
+
+    public void setTipoMaceta(String tipoMaceta) {
+        this.tipoMaceta = tipoMaceta;
+    }
+
 }
